e09a57bf413d8575d3555d4b6155d509be48d2c3
aghe12 <meghashettyj@gmail.com>
2026-06-23 11:16:39 +0530

    added fewchanges in the payment controller test

--- src/payments/payments.controller.spec.ts
+++ src/payments/payments.controller.spec.ts
@@ -24,29 +24,23 @@ describe('PaymentsController', () => {
     // APPROACH 1: NO MOCKING (Testing Controller + Service)
 
     it('should return real data from the actual service', () => {
-      // 1. Arrange: We have to guess the ACTUAL data the real service returns
       const expectedResult = ['payment1', 'payment2', 'payment3'];
 
-      // 2. Act: Call the controller method directly
       const result = controller.getPayments();
 
-      // 3. Assert: Verify it matches the real data
       expect(result).toEqual(expectedResult);
     });
 
     // APPROACH 2: MOCKING (Testing Controller in Isolation)
 
     it('should return fake data using a mocked service', () => {
-      // 1. Arrange: Create totally different fake data for the test
       const expectedResult = ['mocked_payment_1', 'mocked_payment_2'];
 
-      // 2. Mock: Intercept 'findAll' and force it to return our fake array!
+      //Mock: Intercept 'findAll' and force it to return our fake array!
       jest.spyOn(service, 'findAll').mockImplementation(() => expectedResult);
 
-      // 3. Act: Call the controller (it unknowingly calls the mock)
       const result = controller.getPayments();
 
-      // 4. Assert: Check if the controller successfully returned our fake array
       expect(result).toEqual(expectedResult);
     });
   });
